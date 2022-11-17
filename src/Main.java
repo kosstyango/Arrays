338c7380fe8b28bb102719103ef21ee1147dd644
Konstantin Barnaev <konstantin.barnaev@gmail.com>
2022-11-17 06:59:13 +0200

    Array console input & sort Checking for real number

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,38 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Scanner;
+
+import static java.lang.Integer.parseInt;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        ArrayList<Integer> list = new ArrayList<Integer>();
+        ArrayList<Integer> list = new ArrayList<>();
 
         while (true) {
             System.out.println("Input a number, please");
-            String s = reader.readLine();
-            if (s.isEmpty()) break;
 
-            int x = Integer.parseInt(s);
-            if (x % 2 == 0)  //проверяем, что остаток от деления на два равен нулю
-                list.add(x);          //добавление в конец
-            else
-                list.add(0, x);      //вставка в начало
+            String s = reader.readLine();
+            if (s.isEmpty()) break; //проверка на окончание ввода массива
+            int x;
+            boolean isCorrectInput = false;
+
+            while (isCorrectInput == false) { //проверка s на int
+                try {
+                    x = parseInt(s); //пробуем преобразовать строку в число
+                    isCorrectInput = true;
+                    if (x % 2 == 0)  //проверяем, что остаток от деления на два равен нулю
+                        list.add(x);          //добавление в конец
+                    else
+                        list.add(0, x);      //вставка в начало
+                } catch (Exception e) //ошибка "это не число"
+                {
+                    System.out.println("Your input: \"" + s + "\". It is NOT a number. \nPlease, input a number");
+                    s = reader.readLine();
+                }
+            }
+
         }
         System.out.println("Result array is:");
         System.out.println(list);
